41a237adb720e2f7f3b47a6ad404a2ef31fb90a4
Lukaos187 <lukas.jensen@web.de>
2025-01-07 16:21:53 +0100

    REMOVE some stuff

--- src/main/java/util/Visualizer.java
+++ src/main/java/util/Visualizer.java
@@ -2,28 +2,20 @@ package util;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 public class Visualizer {
     public static final int WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = WINDOW_WIDTH * 9 / 16;
     private final SortArray sortArray;
     private final JFrame frame;
-    private int width, height;
 
     public Visualizer(){
         sortArray = new SortArray();
         frame = new JFrame("Sorting visualizer");
         setupFrame();
-        updateWidthAndHeight();
         sortArray.repaint();
     }
 
-    private void updateWidthAndHeight() {
-        width = frame.getWidth();
-        height = frame.getHeight();
-    }
 
     private void setupFrame() {
         frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -34,26 +26,5 @@ public class Visualizer {
         frame.setLayout(null);
         frame.setResizable(false);
         frame.setVisible(true);
-
-        frame.addComponentListener(new ComponentListener() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-               updateWidthAndHeight();
-            }
-            @Override
-            public void componentMoved(ComponentEvent e) {}
-            @Override
-            public void componentShown(ComponentEvent e) {}
-            @Override
-            public void componentHidden(ComponentEvent e) {}
-        });
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
     }
 }
